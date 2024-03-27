af816a7e6f6af806d7b7425600fb659da26431ef
Zuk3s <felipemarcedo_@hotmail.com>
2024-03-27 01:33:00 -0300

    create login authorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
-import bcrypt from "bcryptjs/dist/bcrypt";
+import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
-import { User } from "../models/User.js";
+import User from "../models/User.js";
 
 // REGISTER USER
 export const register = async (request, response) => {
@@ -40,5 +40,23 @@ export const register = async (request, response) => {
       response.status(500),
       response.send({ error: err.message }, "Error on registration")
     );
+  }
+};
+
+// LOGIN IN
+export const login = async (request, response) => {
+  try {
+    const [email, password] = request.body.auth;
+    const user = await User.findOne({ email: email });
+    if (!user) return response.status(400).json("User does not exist.");
+    const isMach = await bcrypt.compare(password, user.password);
+    if (!isMach)
+      return response.status(400).json({ msg: "Invalid credentials" });
+
+    const token = JWT.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    response.status(200).json({ token, user });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
   }
 };
