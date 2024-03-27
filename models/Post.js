65f4b13e7fd9f4a33d9d5069c5e01bc289d68850
Zuk3s <felipemarcedo_@hotmail.com>
2024-03-27 01:34:21 -0300

    create post model file

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const PostScheme = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 30,
+    },
+    lastName: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 30,
+    },
+    location: String,
+    descrption: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: { type: Map, of: Boolean },
+    coments: { types: Array, default: [] },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostScheme);
+export default Post;
