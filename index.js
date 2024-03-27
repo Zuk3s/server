c4f5a4f243fd42a1ae717e264662cb6753ecb62d
Zuk3s <felipemarcedo_@hotmail.com>
2024-03-27 01:31:15 -0300

    Set auth, users, posts routes

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { authRoutes } from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/post.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -39,9 +43,12 @@ const upload = multer({ storage });
 
 //ROUTER WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture", createPost));
 
 //ROUTER
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/post", postRoutes);
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 5000;
